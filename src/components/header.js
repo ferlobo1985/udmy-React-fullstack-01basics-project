561b41686882686841a473764f43d46a5b24167b
ferlobo1985 <fernando.lobo.prez@gmail.com>
2020-11-20 14:58:38 -0300

    More on styling

--- src/components/header.js
+++ src/components/header.js
@@ -3,11 +3,15 @@ import React,{ Component } from 'react';
 class Header extends Component {
 
     state = {
+        active: 'active',
         keywords:''
     }
     
     inputChangeHandler = (event) => {
+        const value =  event.target.value === '' ? 'active' : 'not-active';
+
         this.setState({
+            active:value,
             keywords: event.target.value
         })
     }
@@ -15,7 +19,8 @@ class Header extends Component {
     
     render(){
         return(
-            <header>
+            // <header style={{background: `${this.state.active ? 'red': 'blue'}`}}>
+            <header className={this.state.active}>
                <div className="logo">Logo</div>
                <input
                     onChange={ this.inputChangeHandler }
