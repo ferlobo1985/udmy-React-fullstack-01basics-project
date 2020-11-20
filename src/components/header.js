ef1055110526df7f9295ce8093d46e0d0c565bac
ferlobo1985 <fernando.lobo.prez@gmail.com>
2020-11-20 15:55:18 -0300

    filtering the news

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,14 @@
-import React,{ Component } from 'react';
+import React from 'react';
 
-class Header extends Component {
-
-    state = {
-        active: 'active',
-        keywords:''
-    }
-    
-    inputChangeHandler = (event) => {
-        const value =  event.target.value === '' ? 'active' : 'not-active';
-
-        this.setState({
-            active:value,
-            keywords: event.target.value
-        })
-    }
-
-    
-    render(){
-        return(
-            // <header style={{background: `${this.state.active ? 'red': 'blue'}`}}>
-            <header className={this.state.active}>
-               <div className="logo">Logo</div>
-               <input
-                    onChange={ this.inputChangeHandler }
-               />
-            </header>
-        )
-    }
+const Header = (props) => {
+    return(
+        <header>
+            <div className="logo">Logo</div>
+            <input
+                onChange={props.keywords}
+            />
+        </header>
+    )
 }
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -11,16 +11,30 @@ class App extends Component {
 
     state = { 
         news: JSON,
+        filtered:JSON,
         footerText:'I am a happy footer'
     }
 
+    getKeywords = (event) => {
+        let keywords = event.target.value;
+        let filtered = this.state.news.filter((item)=>{
+            return item.title.indexOf(keywords) > -1;
+        });
+
+        this.setState({
+            filtered
+        });
+    }
+
     render(){
-        const {news,footerText} = this.state;
+        const {news,footerText,filtered} = this.state;
 
         return(
             <>
-                <Header/>
-                <NewsList news={news}>
+                <Header
+                    keywords={this.getKeywords}
+                />
+                <NewsList news={filtered}>
                     <br/>
                     <h1>I am a children</h1>
                 </NewsList>
